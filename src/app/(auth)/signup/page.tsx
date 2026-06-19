de8fcf9652deb3e76b418abe1e198f7d4aeba34e
mgkvibez <adaenonche92@gmail.com>
2026-06-19 07:30:22 +0000

    updates

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,19 +1,7 @@
 "use client";
 
-import { Suspense } from "react";
-import dynamic from "next/dynamic";
-
-const SignupForm = dynamic(
-  () => import("./signup-form").then((mod) => mod.SignupForm),
-  {
-    suspense: true,
-  }
-);
+import { SignupForm } from "./signup-form";
 
 export default function SignupPage() {
-  return (
-    <Suspense fallback={<div className="text-center py-10">Loading sign-up form...</div>}>
-      <SignupForm />
-    </Suspense>
-  );
+  return <SignupForm />;
 }
