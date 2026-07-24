08525800aac3af27deafb0e4b0314827bc1e7d29
mgkvibez <adaenonche92@gmail.com>
2026-07-24 00:30:46 +0000

    refix

--- src/lib/middleware.ts
+++ src/lib/middleware.ts
@@ -1,5 +1,5 @@
 /**
- * DEPRECATED: This middleware has been moved to src/middleware.ts for Next.js to detect it correctly.
+ * DEPRECATED: This middleware has been moved to src/proxy.ts for Next.js to detect it correctly.
  * The logic has been updated to use the correct session cookie for Firebase.
  */
 export {};
