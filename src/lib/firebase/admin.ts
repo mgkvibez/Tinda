cce33c10b8e87d1c4dbd2fd875bdc3e859b7d52a
mgkvibez <adaenonche92@gmail.com>
2026-07-24 21:02:22 +0000

    refix

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -3,13 +3,13 @@ import { initializeApp, getApps, cert } from 'firebase-admin/app'
 import { getFirestore } from 'firebase-admin/firestore'
 import { getAuth } from 'firebase-admin/auth'
 
-if (!process.env.FIREBASE_PROJECT_ID || !process.env.FIREBASE_CLIENT_EMAIL || !process.env.FIREBASE_PRIVATE_KEY) {
+if (!process.env.FIREBASE_PROJECT_ID ||!process.env.FIREBASE_CLIENT_EMAIL ||!process.env.FIREBASE_PRIVATE_KEY) {
   throw new Error('Missing Firebase Admin envs')
 }
 
 const privateKey = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
 
-const adminApp = getApps().length > 0 ? getApps()[0] : initializeApp({
+const adminApp = getApps().length > 0? getApps()[0] : initializeApp({
   credential: cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
